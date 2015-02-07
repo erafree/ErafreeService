21d6efc04b0325e04512230c6bde910b7cc7bd19
kumanik <mojpo4tovik@gmail.com>
2015-02-07 05:28:48 +0300

    page behavior

--- js/promo.js
+++ js/promo.js
@@ -11,6 +11,11 @@ var promopage = function() {
 		$mainB = $('.livehandler', $mS),
 		$messageB = $('.livehandler', $cS),
 		$msForm = $('.pr_form'),
+		$logohandler = $('.logohandler'),
+		$haveInfo = $('.haveInfo'),
+		$cancelContact = $('.pr_back'),
+		$arrow = $('.fr_arrow'),
+		$txtarea = $('textarea', $msForm),
 		isMain = true,
 		animClasses = 'bounceOutLeft bounceOutRight bounceInLeft bounceInRight';
 
@@ -22,25 +27,63 @@ var promopage = function() {
 
 	function bindEvents() {
 		$mainB.on('click', moveContact);
+		$haveInfo.on('click', moveContact);
 		$messageB.on('click', submitMsg);
+		$logohandler.on('click', cancelContact);
+		$cancelContact.on('click', cancelContact);
+		$txtarea.on('keydown. focus', txtenter);
+	}
+
+	function txtenter() {
+		$txtarea.removeClass('error');
+	}
+
+	function cancelContact() {
+		if(!isMain) {
+			doAnim($cS, $mS, 'bounceOutRight', function() {
+			  isMain = true;
+			  txtenter();
+			});
+
+			$arrow
+				.addClass('bounceOutLeft')
+				.bind(utils.animStr, function (e) {
+					$arrow.unbind(utils.animString).removeClass(animClasses);
+				});
+		}
 	}
 
 	function moveContact() {
-		doAnim($mS,$cS,'bounceOutLeft');
+		doAnim($mS,$cS,'bounceOutLeft', function() {
+		  isMain = false;
+		});
 	}
 
 	function submitMsg() {
+		var data = $msForm.serialize();
+
+		if(!$txtarea.val().trim()) {
+			$txtarea.addClass('error');
+			return;
+		}
+		if($messageB.hasClass('disabled')) return;
+		$messageB.addClass('disabled');
+
 		$.ajax({
 	        url: $msForm.attr('action'),
 	        type: 'POST',
-	        data: $msForm.serialize(),
+	        data: data,
 	        success: function(data) {
-	            
+	        	doAnim($cS, $mS, 'bounceOutRight', function() {
+				  isMain = true;
+				  $('input, textarea', $msForm).val('');
+				  $messageB.removeClass('disabled');
+				});
 	        }
 	    });
 	}
 
-	function doAnim($f, $l, cl) {
+	function doAnim($f, $l, cl, cb) {
 		var wH = Math.max($f.height(),$(window).height()),
             wW = $(window).width(),
 			onAnimationFinished = null;window.scroll(0, 0);
@@ -63,7 +106,7 @@ var promopage = function() {
           $('body').removeAttr('style');
 
           onAnimationFinished = null;
-		  isMain = false;
+          cb && cb();
         };
 
 		$sections
